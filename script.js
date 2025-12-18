8d8af600bb19dc55a5fa520541f32154dd6903aa
Paolo Berumen <paolo.berumen@outlook.com>
2025-12-18 16:25:14 -0600

    Add mobile navigation menu

--- script.js
+++ script.js
@@ -31,3 +31,10 @@ serviceButtons.forEach(btn => {
     document.querySelector(`.${target}`).classList.add("active");
   });
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
